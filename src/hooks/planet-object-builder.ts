2c4e5e73d7da9bc8de1c07fd245f42ec8be42bdf
amin.hasanzadeh <amin.hasanzadeh@axon.teh>
2025-09-03 16:15:31 +0330

    Added planetary ring factor

--- src/hooks/planet-object-builder.ts
+++ src/hooks/planet-object-builder.ts
@@ -1,4 +1,5 @@
 import weightedChoice from "./weighted-choice.ts";
+import type {planetRingType} from "../types/planet-type.tsx";
 
 // const planetAttr = {
 //     PLANET_TYPE: 0xA1A1,
@@ -6,18 +7,18 @@ import weightedChoice from "./weighted-choice.ts";
 //     PLANET_DISTANCE: 0xA2B,
 // }
 
-// const BIOME_ALIASES = {
-//     Lush: ["Rainy", "Tropical", "Grassland", "Overgrown", "Swamp", "Boggy"],
-//     Water: ["Ocean", "Island", "Frozen-Ocean"],
-//     Barren: ["Desert", "Dusty", "Wind-Swept", "Magnetic"],
-//     Hot: ["Charred", "Molten", "Lava-Covered"],
-//     Frozen: ["Icy", "Permafrost", "Sub-zero"],
-//     Toxic: ["Poisonous", "Corrosive"],
-//     Irradiated: ["Radioactive", "Nuclear", "Rad-Melting"],
-//     Dead: ["Airless", "Metal-Built"],
-//     Exotic: ["Alien", "Surreal", "Geology"],
-//     Giant: ["Abyssal", "Infinite-Storm"],
-// }
+const BIOME_ALIASES:  Record<string, string[]> = {
+    Lush: ["Rainy", "Tropical", "Grassland", "Overgrown", "Swamp", "Boggy"],
+    Water: ["Ocean", "Island", "Frozen-Ocean"],
+    Barren: ["Desert", "Dusty", "Wind-Swept"],
+    Hot: ["Charred", "Molten", "Lava-Covered", "Ash-Land"],
+    Frozen: ["Icy", "Permafrost", "Sub-zero"],
+    Toxic: ["Poisonous", "Corrosive"],
+    Irradiated: ["Radioactive", "Nuclear", "Rad-Melting"],
+    Dead: ["Airless", "Metal-Built"],
+    Exotic: ["Alien", "Surreal", "Geology", "Magnetic", "Methan-Formed"],
+    Giant: ["Abyssal", "Infinite-Storm"],
+}
 
 export default function planetBuilder(rng: () => number, index: number, temp: number) {
 
@@ -61,17 +62,65 @@ export default function planetBuilder(rng: () => number, index: number, temp: nu
 
     const planetTypeIndex = weightedChoice(rng, planetTypes) - 1
     const chosenPlanet = planetTypes[planetTypeIndex]
+
     const finalSize = chosenPlanet.biome === "Giant" ? 1 : weightedChoice(rng, sizes)
 
     const planetSector = ranges.filter((e) => e.acceptedBiomes.includes(chosenPlanet.biome))[0].range || sectionGap
 
-    return {
-        planet: {
-            biome: chosenPlanet.biome,
-            size: finalSize,
-            distance: initialDistanceFactor + planetSector + ((index * 2)),
-            speed: speedFactor / finalSize / planetSector
-        },
+    const chosenPlanetVariant = BIOME_ALIASES[chosenPlanet.biome]
+        .map((biome) => ({value: biome, weight: 100 / BIOME_ALIASES[chosenPlanet.biome].length}))
+    const chosenPlanetBiome = weightedChoice(rng, chosenPlanetVariant)
+
+    const hasRing = [
+        {value: false, weight: 80},
+        {value: true, weight: 20},
+    ]
+
+    const ringSizes = [
+        {value: finalSize + 0.1, weight: 25},
+        {value: finalSize + 0.2, weight: 25},
+        {value: finalSize + 0.3, weight: 25},
+        {value: finalSize + 0.4, weight: 25},
+    ]
+
+    const ringCounts = [
+        {value: 1, weight: 45},
+        {value: 2, weight: 40},
+        {value: 3, weight: 10},
+        {value: 4, weight: 5},
+    ]
+
+    const ringThickness = [
+        {value: 0.1, weight: 25},
+        {value: 0.2, weight: 25},
+        {value: 0.3, weight: 25},
+        {value: 0.4, weight: 25},
+    ]
+
+    const ringGap = [
+        {value: 0.05, weight: 25},
+        {value: 0.1, weight: 25},
+        {value: 0.2, weight: 25},
+        {value: 0.3, weight: 25},
+    ]
+
+    const planetaryRing: planetRingType = {
+        color: "#bcbc54",
+        size: weightedChoice(rng, ringSizes),
+        count: weightedChoice(rng, ringCounts),
+        ringGapFactor: weightedChoice(rng, ringGap),
+        ringThicknessFactor: weightedChoice(rng, ringThickness),
+        ringSizeFactor: 1,
     }
 
+    return {
+        planet: {
+            type: chosenPlanet.biome,
+            biome: chosenPlanetBiome,
+            size: finalSize,
+            distance: initialDistanceFactor + planetSector + ((index * 2)),
+            speed: speedFactor / finalSize / planetSector,
+            rings: weightedChoice(rng, hasRing) ? planetaryRing : undefined
+        },
+    }
 }
